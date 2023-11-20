9017257554807a4a78395190e3c81c2d5fefdb15
Timur Makimov <timur.makimov@cae.com>
2023-11-20 15:06:31 -0800

    feat: added Order functionality

--- Program.cs
+++ Program.cs
@@ -48,5 +48,7 @@ app.MapHub<DeathlyHallowsHub>("/hubs/deathlyHallows");
 app.MapHub<HouseGroupHub>("/hubs/houseGroupHub");
 app.MapHub<NotificationHub>("/hubs/notificationHub");
 app.MapHub<ChatHub>("/hubs/chat");
+app.MapHub<OrderHub>("/hubs/order");
+
 
 app.Run();
